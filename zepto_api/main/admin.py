9de19dd5891acd41e0fcb5e15f44d7ffcbdce72b
ShesterkinVladimir <creamcakeQWER@yandex.ru>
2021-07-06 23:16:56 +0300

    added models

--- zepto_api/main/admin.py
+++ zepto_api/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Library, Author, Book
 
-# Register your models here.
+# admin.site.register(Library)
+# admin.site.register(Author)
+# admin.site.register(Book)
--- zepto_api/main/models.py
+++ zepto_api/main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 
+class Library(models.Model):
+    address = models.CharField(max_length=256)
+    book_capacity = models.PositiveIntegerField()
+    name = models.CharField(max_length=128)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=128)
+    birth_date = models.DateTimeField(auto_now_add=True)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=128)
+    year = models.SmallIntegerField(null=True)
+    authors = models.ManyToManyField(Author)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE, related_name="library_book")
+
+
+
